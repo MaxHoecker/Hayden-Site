b602d0ff30b9dc908be40d4c26d7aa25e3febb41
RogerTheRabbit <34873229+RogerTheRabbit@users.noreply.github.com>
2024-01-18 23:07:06 -0500

    Cleanup landing page a bit

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -3,17 +3,17 @@ import React from 'react'
 export default function LandingPage() {
   return (
     <div className='min-w-screen w-screen min-h-screen h-screen bg-sky-50 flex justify-center content-center items-center'>
-      <div className='grid grid-cols-2'>
-        <div>
-          <h1 className='text-5xl h-fit'>Hayden Brewer</h1>
+      <div className='grid grid-cols-2 gap-10'>
+        <div className='flex flex-col justify-center'>
+          <h1 className='text-5xl h-fit text-right mb-5'>Hayden Brewer</h1>
           <div className='flex flex-col h-fit text-right'>
-            <a href='/about'>About Me</a>
-            <a href='/works'>My Work</a>
-            <a href='/resume'>Resume</a>
-            <a href='/contact'>Contact Me</a>
+            <a className='text-xl' href='/about'>About Me</a>
+            <a className='text-xl' href='/works'>My Work</a>
+            <a className='text-xl' href='/resume'>Resume</a>
+            <a className='text-xl' href='/contact'>Contact Me</a>
           </div>
-          <img src="./cover.png"/>
         </div>
+          <img className='w-96 rounded-3xl' src="./cover.png"/>
       </div>
     </div>
   )
