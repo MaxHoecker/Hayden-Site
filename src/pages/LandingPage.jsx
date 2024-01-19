7daeefe98a52fd980591e5288ad503782f16f904
RogerTheRabbit <34873229+RogerTheRabbit@users.noreply.github.com>
2024-01-19 00:07:05 -0500

    Make buttons harder to click accidentally

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -6,15 +6,17 @@ export default function LandingPage() {
     <div className='min-w-screen w-screen min-h-screen h-screen bg-backgroundColor flex justify-center content-center items-center'>
       <div className='grid grid-cols-2 gap-10'>
         <div className='flex flex-col justify-center'>
-          <h1 className='text-5xl h-fit text-right mb-5'>Hayden Brewer</h1>
-          <div className='flex flex-col h-fit text-right'>
-            <Link className='text-xl' to='/about'>About Me</Link>
-            <Link className='text-xl' to='/works'>My Work</Link>
-            <Link className='text-xl' to='/resume'>Resume</Link>
-            <Link className='text-xl' to='/contact'>Contact Me</Link>
+          <h1 className='text-5xl text-right mb-5'>Hayden Brewer</h1>
+          <div className='flex flex-col items-end'>
+            <Link className='text-xl w-fit' to='/about'>About Me</Link>
+            <Link className='text-xl w-fit' to='/works'>My Work</Link>
+            <Link className='text-xl w-fit' to='/resume'>Resume</Link>
           </div>
         </div>
-          <img className='w-96 rounded-3xl' src="./cover.png"/>
+        <div className='center'>
+          <img className='w-96 rounded-3xl pb-5' src="./cover.png"/>
+          <a className='text-xl w-full text-center block' href="mailto:hayden.brewer@maine.edu">hayden.brewer@maine.edu</a>
+        </div>
       </div>
     </div>
   )
