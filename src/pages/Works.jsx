92dc4068ba4bbfc8a2077999d3de14271aefb89e
MaxHoecker <hoecker.max@gmail.com>
2024-01-18 23:52:20 -0500

    Elaborated Works

--- src/pages/Works.jsx
+++ src/pages/Works.jsx
@@ -1,21 +1,21 @@
 function Work() {
   return (
-    <div className="h-screen lg:grid grid-cols-4 items-center" >
+    <div className="h-screen p-40 lg:grid grid-cols-4 gap-10 items-center text-xl" >
       <img className="rounded-3xl" src='./work/chair-sketch.png' />
-      <div>
-        Hi
+      <div className="p-5">
+        Dungeon Master Chair: This is for the table top role pay game Dungeons and Dragons. The Dungeon Master sits on their throne setting the mood with the color changing back rest. The danger looms with the swords stabbed into the side of the chair. The endless possibilities in the infinity icosahedron.
       </div>
       <img className="rounded-3xl" src='./work/ring-sketch.png' />
-      <div>
-        Hi
+      <div className="p-5">
+        Adjustable Double Burner Forge: After making a deal to clean out a shop in exchange for help welding and materials, my adjustable double burner forge was plasma cut and welded together.
       </div>
       <img className="rounded-3xl" src='./work/vise-sketch.png' />
-      <div>
-        Hi
+      <div className="p-5">
+        See Glass Ring: My dad never liked how he proposed to my mom and 2023 was their 30th wedding anniversary. I designed the ring in SolidWorks and 3D-printed it using a casting resin. With the help of a jeweler friend, we cast the ring in silver and used sea glass for the stones.
       </div>
       <img className="rounded-3xl" src='./work/forge-sketch.png' />
-      <div>
-        Hi
+      <div className="p-5">
+        V-Grove Bench Vise: This was a 2-year journey to make as we took a class to learn SolidWorks and model a pre-designed steam engine. Next semester we milled and lathed the parts we modeled. Next, we designed our own vise using the knowledge we had. Finally, to put our design to the test we made. A sort of put your money where your mouth is class.
       </div>
     </div>
   )
